5773035a6f78125b7ef1901c9fc9a53edcddb17e
AmrAhmed119 <amreaglegolden@gmail.com>
2025-09-30 01:47:58 +0300

    project setup

---
+++ src/main/java/com/expenshare/Application.java
@@ -0,0 +1,23 @@
+package com.expenshare;
+
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Expen-Share",
+        version = "1.0.0",
+        description = "API documentation for the Expen-Share application, a platform to manage expenses, track sharing, and integrate with Kafka messaging.",
+        contact = @Contact(
+            name = "Amr",
+            url = "https://github.com/AmrAhmed119/expen-share"
+        )
+    )
+)
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
